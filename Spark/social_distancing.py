f51f20723a3f04e76bad737eac8c12aafdbf0edf
Sike Dong <34067523+SikeDong@users.noreply.github.com>
2020-10-09 17:13:49 -0400

    Update comments for code

--- Spark/social_distancing.py
+++ Spark/social_distancing.py
@@ -18,12 +18,12 @@ df_sd = df.select("origin_census_block_group", "date_range_start", "distance_tra
 # cast data columns type from string to integer
 df_sd_num = df_sd.withColumn("distance_traveled_from_home", df_sd["distance_traveled_from_home"].cast('int')).withColumn("median_home_dwell_time", df_sd["median_home_dwell_time"].cast('int')).withColumn("part_time_work_behavior_devices", df_sd["part_time_work_behavior_devices"].cast('int')).withColumn("full_time_work_behavior_devices", df_sd["full_time_work_behavior_devices"].cast('int')).withColumn("median_non_home_dwell_time", df_sd["median_non_home_dwell_time"].cast('int'))
 
-# cast data type to date
+# cast data type of "date_range_start" to date
 df_sd_date = df_sd_num.withColumn("date",to_date(col('date_range_start')).alias('date').cast("date"))
-# substract information from fips code and save into state
+# substract information from fips code("origin_census_block_group" and save into State
 df_sd_state = df_sd_date.withColumn("State", df.origin_census_block_group.substr(0,2))
 
-# scale up data to daily basis, and in different
+# scale up data to daily basis, and in different states
 df_sd_final = df_sd_state.groupBy("date","State").sum("distance_traveled_from_home", "median_home_dwell_time", "part_time_work_behavior_devices", "full_time_work_behavior_devices", "median_non_home_dwell_time")
 
 # write data into database
